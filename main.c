9d728a73ff087457a419a912f250c3e55c06cbf6
Ben <90698574+ndaloben@users.noreply.github.com>
2021-10-09 20:21:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n\t\t\t");
+    printf("\n\t\t\t");
+    printf("LIBRARY SYSTEM\n\t\t\t");
+    printf("\n\t\t\t");
+    printf("Welcome Mr. Ouma Benard!\n\t\t\t");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("MENU\n");
+    printf("Search Book\n");
+    printf("Add Book\n");
+    printf("Edit Book\n");
+    printf("Delete Book\n");
+    printf("Download Book\n");
+    printf("Edit profile\n");
+    printf("Exit\n");
+
+    return 0;
+}
+
