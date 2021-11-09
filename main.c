462f31978ed296dacdc5b7ea163d2600bf4c4608
Ben <90698574+ndaloben@users.noreply.github.com>
2021-11-09 14:09:24 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,38 @@
+
 #include <stdio.h>
 #include <stdlib.h>
+#include<windows.h>
+
+int menu(); // prototype
+int main ()
 
 int main()
-{
-    printf("\n\t\t\t");
-    printf("\n\t\t\t");
-    printf("LIBRARY SYSTEM\n\t\t\t");
-    printf("\n\t\t\t");
-    printf("Welcome Mr. Ouma Benard!\n\t\t\t");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("MENU\n");
-    printf("Search Book\n");
-    printf("Add Book\n");
-    printf("Edit Book\n");
-    printf("Delete Book\n");
-    printf("Download Book\n");
-    printf("Edit profile\n");
-    printf("Exit\n");
 
-    return 0;
+    Action = menu();
+    do {
+    printf("\tcounty Library\n");
+    printf("Welcome Mr.Philip. \n");
+    printf("What would you like to do?\n");
+    printf("1. view Users.\n");
+    printf("2. Add new User\n");
+    printf("3. Edit User. \n");
+    printf("4. Delete User.\n");
+    printf("5. Change password.");
+    printf("6. Logout. \n");
+    printf("7. Exit. \n");
+    printf("Selected action(1-7): ");
+    scanf("%d", &action);
+    if(action<1 || action >7)
+            printf("Invalid Action. Try again");
+        }
+
+    }  while(action< 1 || action > 7);{
+        system ("cls);
+        printf("Invalid Action.Try again.\n");
+    }
+
+        return 0;
+
 }
+
 
